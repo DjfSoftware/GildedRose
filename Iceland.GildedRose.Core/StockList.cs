c016ef7da53a96541bc43fa2f7812c122a74007f
David Forbes <david.forbes@mandogroup.com>
2018-09-25 20:12:07 +0100

    StockList item incorrectly marked as Matured, should be Standard

--- Iceland.GildedRose.Core/StockList.cs
+++ Iceland.GildedRose.Core/StockList.cs
@@ -16,7 +16,7 @@ namespace Iceland.GildedRose.Core
             Add(new Item("Backstage pass #2", 9, 2, ItemType.BackstagePass));
             Add(new Item("Sulfuras", 2, 2, ItemType.Legendary));
             Add(new Item("Normal item #1", -1, 55, ItemType.Standard));
-            Add(new Item("Normal item #2", 2, 2, ItemType.Maturing));
+            Add(new Item("Normal item #2", 2, 2, ItemType.Standard));
             Add(new Item("Invalid item", 2, 2, ItemType.Invalid));
             Add(new Item("Conjured #1", 2, 2, ItemType.Conjured));
             Add(new Item("Conjured #2", -1, 5, ItemType.Conjured));
